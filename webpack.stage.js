893027fb53e4779870c6ae4b460d43bae4310e4a
JodieHag <info@jodiehag.com>
2017-11-09 14:22:49 +0100

    change commands

---
+++ webpack.stage.js
@@ -0,0 +1,23 @@
+const Merge = require('webpack-merge');
+const CommonConfig = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = Merge(CommonConfig, {
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      },
+      API_URL: JSON.stringify('')
+    })
+  ]
+});
